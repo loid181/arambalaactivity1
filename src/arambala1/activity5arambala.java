bc8a1bc49771966fd88bb5b73262d333f39b78c7
JeremyTheGreats <rabanesjeremy@gmail.com>
2025-07-30 23:48:57 -0700

    added attempts

--- src/arambala1/activity5arambala.java
+++ src/arambala1/activity5arambala.java
@@ -1,14 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package arambala1;
 
-/**
- *
- * @author PC 33
- */
+import java.util.Scanner;
+import loidarams.bankingclass;
 public class activity5arambala {
-    
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("HELLO WELCOME TO THE SYSTEM!");
+        System.out.println("What do you feel doing today? ");
+        System.out.println("1. Banking");
+        System.out.println("2. Doctors Appointment");
+        System.out.println("3. Shopping");
+        
+        System.out.print("Enter choice: ");
+        int choice = sc.nextInt();
+        
+        switch(choice){
+            case 1:
+                bankingclass bc = new bankingclass();
+                int attempts = 0;
+                boolean isLoggedIn = false;
+                
+                
+                while(attempts < 3 && !isLoggedIn){
+                    System.out.println("Enter your Account No: ");
+                    int accountNo = sc.nextInt();
+
+                    System.out.println("Enter your Pin: ");
+                    int pin = sc.nextInt();
+
+                    if(bc.verifyAccount(accountNo, pin)){
+                        System.out.println("LOGIN SUCCESS");
+                        isLoggedIn = true; 
+                    } else {
+                        attempts++;
+                        System.out.println("INVALID ACCOUNT! You have " + (3 - attempts) + " attempt(s) remaining.");
+                    }
+
+                    if(attempts == 3) {
+                        System.out.println("Too many failed attempts. Account locked.");
+                    }
+                }
+                
+                break;
+            case 2:
+              
+                break;
+            case 3:
+                
+                
+                break;
+            default:
+                System.out.println("Invalid Selection!");
+        }
+    }
 }
